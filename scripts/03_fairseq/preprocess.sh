02071dc1f1c1c217e88a9deebc827cb7a176a9d4
peerachetporkaew <peerachet.porkaew@gmail.com>
2022-02-16 03:02:58 +0000

    update preprocess script

--- scripts/03_fairseq/preprocess.sh
+++ scripts/03_fairseq/preprocess.sh
@@ -1,3 +1,3 @@
-fairseq-preprocess --destdir output/data-bin \
+python ./fairseq/fairseq_cli/preprocess.py --destdir output/data-bin \
    --source-lang zh --target-lang th --trainpref output/train.bpe \
    --validpref output/valid.bpe --testpref output/test.bpe --workers 4
