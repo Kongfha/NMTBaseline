0bbc2d9236fc55f9ad9ad58e91c65cacdeb3f4de
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2022-02-16 08:21:51 +0700

    add user dir

---
+++ scripts/03_fairseq/train_mytransformer.sh
@@ -0,0 +1,7 @@
+python ./fairseq/train.py output/data-bin --task translation \
+        --max-tokens 4000 --source-lang zh --target-lang th --update-freq 4 --ddp-backend=no_c10d \
+        --user-dir ./userdir/mytransformer --arch mytransformer_base  --label-smoothing 0.1 --fp16 \
+        --optimizer adam --adam-betas '(0.9, 0.98)' \
+        --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000  \
+        --lr 0.0005 --save-interval-updates 500  --keep-interval-updates 5 --keep-last-epochs 5 --max-update 100000 \
+        --dropout 0.1 --max-epoch 60 --criterion label_smoothed_cross_entropy --save-dir output/checkpoints
