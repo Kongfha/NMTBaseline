4482a78188af17cfecc69d0f1dea4842f5a3b18e
Kongfha <84579820+Kongfha@users.noreply.github.com>
2022-09-12 21:09:11 +0700

    Update test.sh

--- scripts/03_fairseq/test.sh
+++ scripts/03_fairseq/test.sh
@@ -1,5 +1,5 @@
 python ./fairseq/fairseq_cli/generate.py output/data-bin \
     --remove-bpe --path output/checkpoints/checkpoint_best.pt --beam 8  --task translation --max-tokens 4096 \
-    --log-format none --gen-subset test --nbest 1 --source-lang zh --target-lang th  | tee output.zh-th.out
+    --log-format none --gen-subset test --nbest 1 --source-lang th --target-lang zh  | tee output.th-zh.out
 
-grep ^H output.zh-th.out | cut -f1,3- | cut -c3- | sort -k1n | cut -f2- > output.zh-th.final
+grep ^H output.th-zh.out | cut -f1,3- | cut -c3- | sort -k1n | cut -f2- > output.th-zh.final
