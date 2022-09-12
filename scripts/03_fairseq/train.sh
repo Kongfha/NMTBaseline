73c63761aac6ff689315c859fa8f241c7e830436
Kongfha <84579820+Kongfha@users.noreply.github.com>
2022-09-12 21:07:35 +0700

    Update train.sh

--- scripts/03_fairseq/train.sh
+++ scripts/03_fairseq/train.sh
@@ -1,5 +1,5 @@
 python ./fairseq/train.py output/data-bin --task translation \
-        --max-tokens 4000 --source-lang zh --target-lang th --update-freq 4 --ddp-backend=no_c10d \
+        --max-tokens 4000 --source-lang th --target-lang zh --update-freq 4 --ddp-backend=no_c10d \
         --arch transformer  --label-smoothing 0.1 --fp16 \
         --optimizer adam --adam-betas '(0.9, 0.98)' \
         --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates 4000  \
